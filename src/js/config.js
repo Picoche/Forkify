95efa2aae3f4eb6fdda53c3e04ee00d1fb2faa03
Fabien Hombert <hombert.fabien@gmail.com>
2022-08-16 14:10:06 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e9adea23-0709-471b-a911-dd575e9eb144';
+export const MODAL_CLOSE_SEC = 2.5;
